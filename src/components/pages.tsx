f5af1f7ac72ba05f09d4571b3c133baa8aafd0cf
n-rogget <nicole.rogget@gmail.com>
2023-10-19 16:01:10 -0300

    Cambios en css e instalación de jest y babel

--- src/components/pages.tsx
+++ src/components/pages.tsx
@@ -1,13 +1,21 @@
 import {Dispatch, SetStateAction} from "react";
 
 interface PaginationProps {
+  //setPage actualiza el estado de la página 
     setPage: Dispatch<SetStateAction<number>>;
+    // page representa el número de la página actual
     page: number;
   }
   
   export default function Pages({ setPage, page }: PaginationProps) {
   return (
     <section className="pages">
+       <button className="btn-first" id="page-initial" onClick={()=>{
+        setPage(1)
+      }}>
+        <i className="fa-solid fa-arrow-left"></i>
+        Inicio
+      </button>
       <button className="btn-bef" id="page-before" onClick={()=>{
         setPage(page > 1 ? page - 1:1)
       }}>
@@ -20,6 +28,12 @@ interface PaginationProps {
       }}>
         Siguiente
         <i className="fa-solid fa-arrow-right"></i>
+      </button>
+      <button className="btn-last" id="page-last" onClick={()=>{
+        setPage(/* Math.max(page) */500)
+      }}>
+        <i className="fa-solid fa-arrow-left"></i>
+        Final
       </button>
     </section>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style/header.css'
 import './style/index.css'
 import App from './app'
 import './style/sideBar.css'
@@ -10,6 +9,5 @@ import './style/pages.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     < App />
-  
   </React.StrictMode>,
 )
